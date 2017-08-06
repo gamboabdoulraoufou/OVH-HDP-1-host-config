3239275be897be6075c3326dc0f4d26df4275a7a
Abdoul <gamboabdoulraouf@gmail.com>
2017-08-07 00:03:42 +0200

    Update create_partitions.sh

--- bash/create_partitions.sh
+++ bash/create_partitions.sh
@@ -1,30 +1,25 @@
 
 #!/bin/bash
 
-lvcreate -L 20G -n lvhadoopvar   VolGroup01
+lvcreate -L 200G -n lvhadoopvar   VolGroup01
 mkfs -t ext4 /dev/VolGroup01/lvhadoopvar 
 mkdir -p  /hadoop/var
 mount /dev/VolGroup01/lvhadoopvar  /hadoop/var
 
-lvcreate -L 20G -n lvhadoopwork   VolGroup01
+lvcreate -L 200G -n lvhadoopwork   VolGroup01
 mkfs -t ext4 /dev/VolGroup01/lvhadoopwork 
 mkdir -p  /hadoop/work
 mount /dev/VolGroup01/lvhadoopwork  /hadoop/work
 
-lvcreate -L 20G -n lvhadoopusr   VolGroup01
+lvcreate -L 100G -n lvhadoopusr   VolGroup01
 mkfs -t ext4 /dev/VolGroup01/lvhadoopusr 
 mkdir -p  /hadoop/usr
 mount /dev/VolGroup01/lvhadoopusr  /hadoop/usr
 
-lvcreate -L 20G -n lvhmysql   VolGroup01
-mkfs -t ext4 /dev/VolGroup01/lvhmysql 
-mkdir -p  /hadoop/var/lib/mysql
-mount /dev/VolGroup01/lvhmysql  /hadoop/var/lib/mysql
-
 
 if [ "$1" = "yes" ];
   then
-    lvcreate -L 100G -n lvhadoopdata   VolGroup01
+    lvcreate -L 1500G -n lvhadoopdata   VolGroup01
     mkfs -t ext4 /dev/VolGroup01/lvhadoopdata
     mkdir -p  /hadoop/data/hdfs/01
     mount /dev/VolGroup01/lvhadoopdata  /hadoop/data/hdfs/01
